dc85b1d5d8531998680fe2d922eaec26feaab76b
manas <mddevs21@gmail.com>
2026-02-22 19:11:00 +0530

    retry removed noise

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,10 @@
 import { Container } from '../../components/container'
-import { Noise } from '../../components/noise'
 import React from 'react'
 
 export default function Home() {
   return (
     <div className='min-h-screen relative font-mono'>
-        <Noise />
-        <Container className='relative z-20  h-screen pt-16 max-w-3xl mx-auto'>
+        <Container className='relative z-10  h-screen pt-16 max-w-3xl mx-auto'>
             <div>here i am gonna yap so much that your ears gonna bleed ,
               nah just joking its gonna be some quality stuff 
             </div>
